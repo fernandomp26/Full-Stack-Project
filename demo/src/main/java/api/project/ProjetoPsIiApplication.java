50cb2bcd123f5137d84ea888e7b32b05e084a0aa
Fernando Morales Peres <142750804+fernandomp26@users.noreply.github.com>
2024-04-18 19:43:53 -0300

    Add files via upload

---
+++ demo/src/main/java/api/project/ProjetoPsIiApplication.java
@@ -0,0 +1,13 @@
+package api.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPsIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPsIiApplication.class, args);
+	}
+
+}
