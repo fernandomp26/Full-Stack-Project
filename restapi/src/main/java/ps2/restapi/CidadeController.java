12a4aa67439e1fb7efdce1854910157ca16c2705
Fernando Morales Peres <142750804+fernandomp26@users.noreply.github.com>
2024-04-25 19:43:08 -0300

    Update CidadeController.java

--- restapi/src/main/java/ps2/restapi/CidadeController.java
+++ restapi/src/main/java/ps2/restapi/CidadeController.java
@@ -3,7 +3,6 @@ package ps2.restapi;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
